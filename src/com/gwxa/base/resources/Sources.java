0474f7aa6acb0ac83520ffabc95a7eebea2f30dc
tracy <tracy@DESKTOP-MPMGLLT>
2018-01-05 15:14:14 +0800

    提交图片处理

---
+++ src/com/gwxa/base/resources/Sources.java
@@ -0,0 +1,9 @@
+package com.gwxa.base.resources;
+
+import javafx.scene.image.Image;
+
+public class Sources {
+	public static Image getImage(String path) {
+		return new Image(Resources.class.getResourceAsStream(path));
+	}
+}
